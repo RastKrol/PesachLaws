f84e872704a291815366f92b2715b6d26bd17b73
Rastsislau Krol <lorkmun@outlook.com>
2021-03-12 12:32:41 +0200

    added farmacy section and navigation script

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const sidebar = document.getElementById("sidebar");
 const sidebar_small = document.getElementById("sidebar-small");
 const link_list = sidebar_small.querySelectorAll('li');
 const linkArr = Array.from(link_list);
+const erev = document.getElementById('erev');
+const trufot = document.getElementById('trufot');
+console.log(linkArr[0].children[0].children[0].innerHTML) ;
 
 
 logo1.addEventListener('click', () => {
@@ -28,6 +31,49 @@ const handleClick = (e) => {
     item.classList.remove("side-nav__item--active");
   });
   e.currentTarget.classList.add("side-nav__item--active");
+
+/*   function composedPath(el)  {
+    const path = [];
+
+    while (el) {
+      path.push(el);
+
+      if (el.tagName === "HTML") {
+        path.push(document);
+        path.push(window);
+
+        return path[0];
+      }
+
+      el = el.parentElement;
+    }
+  } */
+
+  if (
+    // e.path[0].innerHTML === "Эрев Песах" 
+    e.path[0].children[0].innerHTML === "Эрев Песах"
+  ) {
+    if (erev.classList.contains("is-close")) {
+      erev.classList.remove("is-close");
+      trufot.classList.add("is-close");
+    } else {
+      trufot.classList.add("is-close");
+      erev.classList.remove("is-close");
+    }
+  }
+
+  if (
+    // e.path[0].innerHTML === "Законы Лекарств" 
+    e.path[0].children[0].innerHTML === "Законы Лекарств"
+  ) {
+    if (trufot.classList.contains("is-close")) {
+      trufot.classList.remove("is-close");
+      erev.classList.add("is-close");
+    } else {
+      trufot.classList.add("is-close");
+      erev.classList.remove("is-close");
+    }
+  }
 };
 
 linkArr.forEach((item) => {
