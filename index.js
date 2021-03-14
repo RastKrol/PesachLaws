0e2417bba0e73b194e9fa31a8cbfd08cf5a14466
Rastsislau Krol <lorkmun@outlook.com>
2021-03-14 04:13:56 +0200

    added koshering kitchen section

--- index.js
+++ index.js
@@ -2,13 +2,90 @@
 const logo1 = document.getElementById("logo1");
 const logo2 = document.getElementById("logo2");
 const sidebar = document.getElementById("sidebar");
-const sidebar_small = document.getElementById("sidebar-small");
-const link_list = sidebar_small.querySelectorAll('li');
+const link_list = sidebar.querySelectorAll('li');
 const linkArr = Array.from(link_list);
+const sidebar_small = document.getElementById("sidebar-small");
+const link_list_small = sidebar_small.querySelectorAll('li');
+const linkArrSmall = Array.from(link_list_small);
 const erev = document.getElementById('erev');
 const trufot = document.getElementById('trufot');
+const koshering = document.getElementById("koshering");
+const sections = document.querySelectorAll(".section");
+const sectionArr = Array.from(sections);
 console.log(linkArr[0].children[0].children[0].innerHTML) ;
+console.log(linkArr[0].classList.contains("side-nav__item--active"));
+console.log(sectionArr);
 
+
+/* ******************************* */
+//*FUNCTIONS 
+/* ******************************* */
+const isNotClose = (button) => {
+  sections.forEach((item) => {
+    if (!item.classList.contains("is-close")) {
+      item.classList.add("is-close");
+      button.classList.remove("is-close");
+    }
+  });
+}
+
+// (function sortList() {
+//   let list, i, switching, b, shouldSwitch;
+//   list = document.getElementById("kosher-list");
+//   switching = true;
+//   /* Make a loop that will continue until
+//       no switching has been done: */
+//   while (switching) {
+//     // start by saying: no switching is done:
+//     switching = false;
+//     b = list.getElementsByTagName("LI");
+//     // Loop through all list-items:
+//     for (i = 0; i < b.length - 1; i++) {
+//       // start by saying there should be no switching:
+//       shouldSwitch = false;
+//       /* check if the next item should
+//           switch place with the current item: */
+//       if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+//         /* if next item is alphabetically
+//             lower than current item, mark as a switch
+//             and break the loop: */
+//         shouldSwitch = true;
+//         break;
+//       }
+//     }
+//     if (shouldSwitch) {
+//       /* If a switch has been marked, make the switch
+//           and mark the switch as done: */
+//       b[i].parentNode.insertBefore(b[i + 1], b[i]);
+//       switching = true;
+//     }
+//   }
+// })();
+
+(function sortUnorderedList(sortDescending) {
+  const list = document.getElementById("kosher-list");
+
+  // Get the list items and setup an array for sorting
+  const lis = list.getElementsByTagName("LI");
+  const vals = [];
+
+  // Populate the array
+  for (var i = 0, l = lis.length; i < l; i++) vals.push(lis[i].innerHTML);
+
+  // Sort it
+  vals.sort();
+
+  // Sometimes you gotta DESC
+  if (sortDescending) vals.reverse();
+
+  // Change the list on the page
+  for (var i = 0, l = lis.length; i < l; i++) lis[i].innerHTML = vals[i];
+})();
+
+
+/* ***************************** */
+//*EVENTS
+/* ***************************** */
 
 logo1.addEventListener('click', () => {
   if (sidebar_small.classList.contains("is-close")) {
@@ -32,26 +109,31 @@ const handleClick = (e) => {
   });
   e.currentTarget.classList.add("side-nav__item--active");
 
+  linkArrSmall.forEach((item) => {
+    item.classList.remove("side-nav__item--active");
+  });
+  e.currentTarget.classList.add("side-nav__item--active");
+
   if (
     e.target.children[0].innerHTML === "Эрев Песах"
   ) {
-    if (erev.classList.contains("is-close")) {
-      erev.classList.remove("is-close");
-      trufot.classList.add("is-close");
-    }
+    isNotClose(erev);
   }
   
-
   if (
     e.target.children[0].innerHTML === "Законы Лекарств"
   ) {
-    if (trufot.classList.contains("is-close")) {
-      trufot.classList.remove("is-close");
-      erev.classList.add("is-close");
-    }
+    isNotClose(trufot);
+  }
+
+  if (e.target.children[0].innerHTML === "Кошерование посуды") {
+    isNotClose(koshering);
   }
 };
 
 linkArr.forEach((item) => {
+  item.addEventListener("click", handleClick);
+});
+linkArrSmall.forEach((item) => {
   item.addEventListener("click", handleClick);
 });
